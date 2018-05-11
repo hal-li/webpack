e86ce91c4700fbb17c2213a5a616a7fafe303102
halli <191318096@qq.com>
2018-05-11 17:14:43 +0800

    修改登录逻辑

--- template/src/mock/login.js
+++ template/src/mock/login.js
@@ -8,6 +8,7 @@ const chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
 const userMap = {
     admin: {
         token: "",
+        password: "111111",
         nickname: "管理员",
         introduction: "我是管理员"
     }
@@ -31,10 +32,12 @@ function responseSuccess(data) {
 export default {
     login: config => {
         // 获取body中的参数，因为请求前序列化参数，后台捕捉到是参数序列化字符串，这里需要反序列化
-        let { username } = unit.deserialize(config.body);
+        let { username, password } = unit.deserialize(config.body);
         // 判断用户是否存在，不在请求失败
         if(!userMap.hasOwnProperty(username))
             return responseError("找不到该用户");
+        if(password !== userMap[ username ].password)
+            return responseError("您输入的密码有误");
         // 反之请求成功,返回随机字符串
         let data = userMap[ username ];
         for (let i = 0; i < 32; i++) {
