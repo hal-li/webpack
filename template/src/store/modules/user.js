58f56a2876288375aa69d1a559d6abaab606dacf
halli <191318096@qq.com>
2018-05-11 14:46:31 +0800

    根据mock模拟的接口完成用户相关的业务开发

--- template/src/store/modules/user.js
+++ template/src/store/modules/user.js
@@ -1,23 +1,28 @@
-import { login } from "../../api/auth"
-import { setToken, getToken } from "@/common/user.service";
+import { login, getUserInfo, logout } from "../../api/auth"
+import { setToken, getToken, removeToken } from "@/common/user.service";
 
 const user = {
     state: {
         token: getToken(),
+        userInfo: null,
     },
 
     mutations: {
         SET_AUTH_TOKEN: (state, token) => {
-            state.token = token
+            state.token = token;
         },
+        SET_USER_INFO: (state, userInfo) => {
+            state.userInfo = userInfo;
+        }
     },
 
     actions: {
         // 常规登录
         login({ commit }, userInfo) {
             const username = userInfo.username.trim();
+            const password = userInfo.password.trim();
             return new Promise((resolve, reject) => {
-                login(userInfo.username, userInfo.password)
+                login(username, password)
                     .then(response => {
                         const data = response.data;
                         const status = response.status;
@@ -29,18 +34,39 @@ const user = {
                         commit('SET_AUTH_TOKEN', data.token);
                         resolve();
                     }).catch(error => {
+                        reject(error);
+                    })
+            })
+        },
+
+        // 获取用户信息
+        getUserInfo({ commit }) {
+            return new Promise((resolve, reject) => {
+                getUserInfo().then(response => {
+                    const data = response.data;
+                    const status = response.status;
+                    if(status !== 1) {
+                        return reject();
+                    }
+                    commit('SET_USER_INFO', data);
+                    resolve();
+                }).catch(error => {
                     reject(error);
                 })
             })
         },
 
-        // 获取用户信息
-        getUserInfo() {
-            return new Promise((resolve, reject) => {
-                // do something ...
-                resolve();
+        // 退出登录，清空缓存
+        logout({ commit }) {
+            return new Promise((resolve) => {
+                logout().then(() => {
+                    commit('SET_AUTH_TOKEN', '');
+                    commit('SET_USER_INFO', null);
+                    removeToken();
+                    resolve();
+                })
             })
-        }
+        },
     }
 };
 
