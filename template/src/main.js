1573be6dff16a1dc0805d44705d54c77fdb8b6ae
halli <191318096@qq.com>
2018-05-11 14:55:05 +0800

    导入接口你看、引入路由拦截器

--- template/src/main.js
+++ template/src/main.js
@@ -4,7 +4,18 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+// 接口模拟，实际开发请勿导入
+import './mock'
+
 Vue.config.productionTip = false;
+
+// 免登录校验白名单，名单内路由，拦截器不予拦截
+const whiteList = [ '/login' ];
+
+// 路由拦截器
+router.beforeEach((to, from, next) => {
+    next();
+});
 
 new Vue({
     el: '#app',
